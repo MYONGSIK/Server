5f82d4c7b54693bc33e3fc479819e2af8766c394
dldmsql <yhy5913@gmail.com>
2023-02-21 22:26:03 +0900

    유저 테이블명 members로 수정

--- src/main/java/com/example/myongsick/domain/user/entity/User.java
+++ src/main/java/com/example/myongsick/domain/user/entity/User.java
@@ -5,6 +5,7 @@ import com.example.myongsick.domain.scrap.entity.Scrap;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,12 +19,13 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@Table(name = "user")
+@Table(name = "members")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
   private Long id;
 
   private String phoneId;
