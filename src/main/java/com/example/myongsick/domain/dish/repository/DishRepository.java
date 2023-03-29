3223944d17f5fefe24f8ce0000118fa1ce53391d
dldmsql <yhy5913@gmail.com>
2023-03-30 01:31:38 +0900

    몽고디비 설정 임시 주석 처리

--- src/main/java/com/example/myongsick/domain/dish/repository/DishRepository.java
+++ src/main/java/com/example/myongsick/domain/dish/repository/DishRepository.java
@@ -1,10 +1,10 @@
-package com.example.myongsick.domain.dish.repository;
-
-import com.example.myongsick.domain.dish.entity.Dish;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DishRepository extends MongoRepository<Dish, String> {
-
-}
+//package com.example.myongsick.domain.dish.repository;
+//
+//import com.example.myongsick.domain.dish.entity.Dish;
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface DishRepository extends MongoRepository<Dish, String> {
+//
+//}
