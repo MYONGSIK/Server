9c8de0283211688ce1bcc92a89a8ca985bbbbe93
dldmsql <yhy5913@gmail.com>
2023-04-27 10:31:35 +0900

    맛집조회 시, 찜꽁 수 포함 가게 데이터 조회 쿼리 작성
    동적 정렬(인기순, 거리순) 적용

---
+++ src/main/java/com/example/myongsick/domain/scrap/repository/ScrapRepositoryCustom.java
@@ -0,0 +1,10 @@
+package com.example.myongsick.domain.scrap.repository;
+
+import com.example.myongsick.domain.scrap.dto.ScrapCountResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
+
+public interface ScrapRepositoryCustom {
+  Page<ScrapCountResponse> findAllByCampusWithPaging(@Param("campus") String campus, Pageable pageable);
+}
